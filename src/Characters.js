de3b8ad2e1e3727db0a014fdf4a51048e972a8dd
Lewis Williams <lewiswilliams1501@gmail.com>
2019-04-04 22:49:39 +0100

    Const query

--- src/Characters.js
+++ src/Characters.js
@@ -3,20 +3,20 @@ import { Query } from "react-apollo";
 import gql from "graphql-tag";
 import Character from "./Character";
 
+const GET_CHARACTERS = gql`
+    query {
+        characters{
+            results{
+                name
+                id
+                image
+            }
+        }
+    }`;
+
+
 export const Characters = () => (
-    <Query
-        query={gql`
-      query{
-  characters{
-    results{
-      name
-      id
-      image
-    }
-  }
-}
-    `}
-    >
+    <Query query={GET_CHARACTERS}>
         {({ loading, data }) => {
             if (loading) return <p>Loading...</p>;
             return data.characters.results.map(p => (
